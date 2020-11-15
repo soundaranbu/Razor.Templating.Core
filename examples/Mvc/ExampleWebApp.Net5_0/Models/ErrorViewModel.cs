8208cd0245b6bffb5d0bd3557dbb909b0e7e7530
soundaranbu <soundaranbu@gmail.com>
2020-11-15 17:13:48 +0530

    moved example projects to it's category folder. added more example project for .net 5 for web, console, api

---
+++ examples/Mvc/ExampleWebApp.Net5_0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExampleWebApp.Net5_0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
