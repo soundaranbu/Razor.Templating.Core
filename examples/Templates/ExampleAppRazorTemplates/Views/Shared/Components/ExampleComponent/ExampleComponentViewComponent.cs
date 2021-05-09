5cf0867104e1ac47544a0c1f53f43bf7d0a80526
Soundar Anbalagan <soundaranbu@gmail.com>
2021-05-09 22:59:42 +0530

    added view component template

---
+++ examples/Templates/ExampleAppRazorTemplates/Views/Shared/Components/ExampleComponent/ExampleComponentViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleRazorTemplatesLibrary
+{
+    public class ExampleComponentViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
