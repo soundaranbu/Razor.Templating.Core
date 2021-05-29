6ccb826857f3fb8eba223718085ed6580f6ba1ea
Soundar Anbalagan <soundaranbu@gmail.com>
2021-05-29 21:38:18 +0530

    #14 Added example to generate PDF out of HTML using Razor.Templating.Core & jsreport library

---
+++ examples/_RealWorldSamples/HtmlToPdf/HtmlToPdf.Api/Controllers/PdfController.cs
@@ -0,0 +1,54 @@
+﻿using jsreport.Binary;
+using jsreport.Local;
+using jsreport.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Razor.Templating.Core;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PdfController : ControllerBase
+    {
+        private readonly ILogger<PdfController> _logger;
+        public PdfController(ILogger<PdfController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            var html = await RazorTemplateEngine.RenderAsync("~/Views/PdfTemplate.cshtml");
+
+            var rs = new LocalReporting()
+                        .KillRunningJsReportProcesses()
+                        .UseBinary(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? JsReportBinary.GetBinary() : jsreport.Binary.Linux.JsReportBinary.GetBinary())
+                        .Configure(cfg => cfg.AllowedLocalFilesAccess().FileSystemStore().BaseUrlAsWorkingDirectory())
+                        .AsUtility()
+                        .Create();
+
+            var generatedPdf = await rs.RenderAsync(new RenderRequest
+            {
+                Template = new Template
+                {
+                    Recipe = Recipe.ChromePdf,
+                    Engine = Engine.None,
+                    Content = html,
+                    Chrome = new Chrome
+                    {
+                        MarginTop = "10",
+                        MarginBottom = "10",
+                        MarginLeft = "50",
+                        MarginRight = "50"
+                    }
+                }
+            });
+
+            return File(generatedPdf.Content, generatedPdf.Meta.ContentType, "GeneratedPdfFile.pdf");
+        }
+    }
+}
