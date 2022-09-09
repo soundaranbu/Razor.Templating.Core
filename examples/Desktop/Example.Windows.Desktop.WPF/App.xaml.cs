05dee386d248d6fbca871cba81b1882fd736d045
Phil Bolduc <pbolduc@users.noreply.github.com>
2022-09-09 08:37:27 -0700

    Revert DI changes in WPF to restore to orignal example

--- examples/Desktop/Example.Windows.Desktop.WPF/App.xaml.cs
+++ examples/Desktop/Example.Windows.Desktop.WPF/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Example.Windows.Desktop.WPF
 {
@@ -9,33 +7,5 @@ namespace Example.Windows.Desktop.WPF
     /// </summary>
     public partial class App : Application
     {
-        public static IHost AppHost { get; private set; }
-
-        public App()
-        {
-            AppHost = Host.CreateDefaultBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddSingleton<MainWindow>();
-                    services.AddRazorTemplating();
-                })
-                .Build();
-        }
-
-        protected override async void OnStartup(StartupEventArgs e)
-        {
-            await AppHost.StartAsync();
-
-            var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
-            mainWindow.Show();
-
-            base.OnStartup(e);
-        }
-
-        protected override async void OnExit(ExitEventArgs e)
-        {
-            await AppHost.StopAsync();
-            base.OnExit(e);
-        }
     }
 }
