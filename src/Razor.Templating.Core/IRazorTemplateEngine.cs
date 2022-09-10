06b2f53b583b5ef4f6c3cc7c9f53fec9a5d64429
Soundar Anbalagan <SOUNDARANBU@GMAIL.COM>
2022-09-10 21:05:35 +0530

    Code cleanup

--- src/Razor.Templating.Core/IRazorTemplateEngine.cs
+++ src/Razor.Templating.Core/IRazorTemplateEngine.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Razor.Templating.Core
@@ -9,11 +8,10 @@ namespace Razor.Templating.Core
         /// <summary>
         /// Renders View(.cshtml) To String
         /// </summary>
-        /// <typeparam name="TModel"></typeparam>
         /// <param name="viewName">Relative path of the .cshtml view. Eg:  /Views/YourView.cshtml or ~/Views/YourView.cshtml</param>
         /// <param name="model">Optional model data</param>
-        /// <param name="viewData">Optional view data</param>
+        /// <param name="viewBagOrViewData">Optional view bag or view data</param>
         /// <returns></returns>
-        Task<string> RenderAsync([DisallowNull] string viewName, object? model = null, Dictionary<string, object>? viewData = null);
+        Task<string> RenderAsync(string viewName, object? model = null, Dictionary<string, object>? viewBagOrViewData = null);
     }
 }
--- src/Razor.Templating.Core/RazorTemplateEngine.cs
+++ src/Razor.Templating.Core/RazorTemplateEngine.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Razor.Templating.Core
 {
     public static class RazorTemplateEngine
     {
-        private static readonly Lazy<IRazorTemplateEngine> _instance = new(CreateInstance, true);
+        private static readonly Lazy<IRazorTemplateEngine> Instance = new(CreateInstance, true);
 
         /// <summary>
         /// Creates an instance of <see cref="RazorTemplateEngine"/> using an internal <see cref="ServiceCollection"/>.
@@ -19,21 +18,20 @@ namespace Razor.Templating.Core
             ServiceCollection services = new();
             services.AddRazorTemplating();
 
-            RazorTemplateEngineImpl instance = new (services.BuildServiceProvider());
+            RazorTemplateEngineImpl instance = new(services.BuildServiceProvider());
             return instance;
         }
 
         /// <summary>
         /// Renders View(.cshtml) To String
         /// </summary>
-        /// <typeparam name="TModel"></typeparam>
         /// <param name="viewName">Relative path of the .cshtml view. Eg:  /Views/YourView.cshtml or ~/Views/YourView.cshtml</param>
         /// <param name="model">Optional model data</param>
-        /// <param name="viewData">Optional view data</param>
+        /// <param name="viewBagOrViewData">Optional view bag or view data</param>
         /// <returns></returns>
-        public async static Task<string> RenderAsync([DisallowNull] string viewName, object? model = null, Dictionary<string, object>? viewData = null)
+        public async static Task<string> RenderAsync(string viewName, object? model = null, Dictionary<string, object>? viewBagOrViewData = null)
         {
-            return await _instance.Value.RenderAsync(viewName, model, viewData).ConfigureAwait(false);
+            return await Instance.Value.RenderAsync(viewName, model, viewBagOrViewData).ConfigureAwait(false);
         }
     }
 }
