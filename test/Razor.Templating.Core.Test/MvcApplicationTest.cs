5f4d0ea92499a3f9ab9da76dcc49c856b58edb8a
Soundar Anbalagan <SOUNDARANBU@GMAIL.COM>
2024-08-06 23:37:57 +0100

    Added test cases for view localization

--- test/Razor.Templating.Core.Test/MvcApplicationTest.cs
+++ test/Razor.Templating.Core.Test/MvcApplicationTest.cs
@@ -29,4 +29,23 @@ public class MvcApplicationTest
         Assert.Contains(@"<li><a href=""/home/Index"">Render content by using only the view name instead of path</a></li>", pageHtml);
         Assert.Contains(@"<h1>This is a partial page</h1>", pageHtml);
     }
+
+    [Theory]
+    [InlineData("fr", "<div>Content from Index.fr.cshtml</div>")]
+    [InlineData("en-US", "<div>Content from Index.cshtml</div>")]
+    public async Task RequestWithCulture_Should_RenderLocalizedView(string culture, string expectedContent)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"/Home/Index?culture={culture}");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        var pageHtml = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains(expectedContent, pageHtml);
+    }
 }
