175334d7bdb6b5eec1bc0f20254e6ebb7813f3e4
Soundar Anbalagan <SOUNDARANBU@GMAIL.COM>
2024-08-07 00:00:56 +0100

    Code refactoring for the sample console app

--- examples/Console/ExampleConsoleApp.Net6_0/Program.cs
+++ examples/Console/ExampleConsoleApp.Net6_0/Program.cs
@@ -13,41 +13,9 @@ namespace ExampleConsoleApp.Net6_0
             {
                 Console.WriteLine(DateTime.Now);
 
-                var model = new ExampleModel()
-                {
-                    PlainText = "Some text",
-                    HtmlContent = "<em>Some emphasized text</em>"
-                };
-
-                var viewData = new Dictionary<string, object>();
-                viewData["Value1"] = "1";
-                viewData["Value2"] = "2";
-
-                var html = await RazorTemplateEngine.RenderAsync("/Views/ExampleView.cshtml", model, viewData);
-                Console.Write(html);
-                Console.WriteLine(DateTime.Now);
-
-                // Render View with View Component
-                html = await RazorTemplateEngine.RenderAsync("/Views/ExampleViewWithViewComponent.cshtml");
-                Console.Write(html);
-
-                // Use service collection
-                // Arrange
-                var model1 = new ExampleModel()
-                {
-                    PlainText = "Lorem Ipsium",
-                    HtmlContent = "<em>Lorem Ipsium</em>"
-                };
-
-                // Add dependencies to the service collection and add razor templating to the collection
-                var services = new ServiceCollection();
-                services.AddTransient<ExampleService>();
-                // Add after registering all dependencies
-                // this is important for the razor template engine to find the injected services
-                services.AddRazorTemplating();
-                // Act
-                var html1 = await RazorTemplateEngine.RenderAsync("~/Views/ExampleViewServiceInjection.cshtml", model);
-                Console.WriteLine(html1);
+                await RenderViewWithModelAsync();
+                await RenderViewComponentWithoutModelAsync();
+                await RenderWithDependencyInjectionAsync();
 
                 Console.WriteLine(DateTime.Now);
             }
@@ -58,6 +26,50 @@ namespace ExampleConsoleApp.Net6_0
             }
 
             Console.ReadLine();
+        }
+
+        private static async Task RenderViewComponentWithoutModelAsync()
+        {
+            // Render View with View Component
+            var html = await RazorTemplateEngine.RenderAsync("/Views/ExampleViewWithViewComponent.cshtml");
+            Console.Write(html);
+        }
+
+        private static async Task RenderViewWithModelAsync()
+        {
+            var model = new ExampleModel()
+            {
+                PlainText = "Some text",
+                HtmlContent = "<em>Some emphasized text</em>"
+            };
+            var viewData = new Dictionary<string, object>();
+            viewData["Value1"] = "1";
+            viewData["Value2"] = "2";
+
+            var html = await RazorTemplateEngine.RenderAsync("/Views/ExampleView.cshtml", model, viewData);
+            Console.Write(html);
+            Console.WriteLine(DateTime.Now);
+        }
+
+        private static async Task RenderWithDependencyInjectionAsync()
+        {
+            // Use service collection
+            // Arrange
+            var model = new ExampleModel()
+            {
+                PlainText = "Lorem Ipsium",
+                HtmlContent = "<em>Lorem Ipsium</em>"
+            };
+
+            // Add dependencies to the service collection and add razor templating to the collection
+            var services = new ServiceCollection();
+            services.AddTransient<ExampleService>();
+            // Add after registering all dependencies
+            // this is important for the razor template engine to find the injected services
+            services.AddRazorTemplating();
+            // Act
+            var html1 = await RazorTemplateEngine.RenderAsync("~/Views/ExampleViewServiceInjection.cshtml", model);
+            Console.WriteLine(html1);
         }
     }
 }
