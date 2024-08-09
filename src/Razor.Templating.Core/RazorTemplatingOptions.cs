ad82ce8bbcc897281a19426803a066a8f1150961
Soundar Anbalagan <SOUNDARANBU@GMAIL.COM>
2024-08-09 16:59:59 +0100

    Remove unsed files

--- src/Razor.Templating.Core/RazorTemplatingOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
-
-namespace Razor.Templating.Core
-{
-    public class RazorTemplatingOptions
-    {
-        public MvcRazorRuntimeCompilationOptions MvcRazorRuntimeCompilationOptions { get; } = new MvcRazorRuntimeCompilationOptions();
-    }
-}
