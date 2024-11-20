fc17bccfb204b2ef32efd150b38c40b89c43321c
Soundar Anbalagan <SOUNDARANBU@GMAIL.COM>
2024-11-20 17:49:04 +0000

    Run tests on .NET 9 (#77)
    * Run tests on .NET 9
    
    * Perf benchmark

---
+++ src/Razor.Template.Core.PerfBenchmark/Program.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ExampleRazorTemplatesLibrary.Models;
+using Razor.Templating.Core;
+
+BenchmarkRunner.Run<RazorTemplateBenchMark>();
+
+[MemoryDiagnoser]
+public class RazorTemplateBenchMark
+{
+
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public async Task RenderViewWithModelAsync()
+    {
+        var model = new ExampleModel()
+        {
+            PlainText = "Some text",
+            HtmlContent = "<em>Some emphasized text</em>"
+        };
+        var viewData = new Dictionary<string, object>();
+        viewData["Value1"] = "1";
+        viewData["Value2"] = "2";
+
+        var html = await RazorTemplateEngine.RenderAsync("/Views/ExampleView.cshtml", model, viewData);
+    }
+}
