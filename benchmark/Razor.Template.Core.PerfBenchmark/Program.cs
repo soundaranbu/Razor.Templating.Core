7548da9537b8cb69b978ac50ff7434d0ec6341f8
Joel Reinford <joel.reinford+gitk@gmail.com>
2026-01-30 21:41:42 -0500

    Modify Benchmark project to include a run for the embedded resource
    In practical terms, the embedded resource is equivalent to RCL
    
    The RazorTemplatesLibraryProjectMarker class is just to provide an easy way to identify an assembly

--- benchmark/Razor.Template.Core.PerfBenchmark/Program.cs
+++ benchmark/Razor.Template.Core.PerfBenchmark/Program.cs
@@ -3,7 +3,8 @@ using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Running;
-using ExampleRazorTemplatesLibrary.Models;
+using EmbeddedResourceTemplates;
+using ExampleRazorTemplatesLibrary;
 using Razor.Templating.Core;
 
 var config = ManualConfig.CreateMinimumViable()
@@ -16,18 +17,68 @@ BenchmarkRunner.Run<RazorTemplatingCoreBenchMark>(config);
 [MarkdownExporterAttribute.GitHub]
 public class RazorTemplatingCoreBenchMark
 {
+    [GlobalSetup]
+    public async Task GlobalSetup()
+    {
+        // Ensure both assemblies are loaded before RazorTemplateEngine initializes
+        _ = typeof(EmbeddedResourceProjectMarker).Assembly;
+        _ = typeof(RazorTemplatesLibraryProjectMarker).Assembly;
+
+        // Warm up the engine by rendering both views once
+        // This ensures all file providers (including embedded) are registered
+        var rclModel = BuildRazorTemplateModel();
+        var embeddedModel = BuildEmbeddedResourceModel();
+
+        var viewData = new Dictionary<string, object>
+        {
+            ["Value1"] = "1",
+            ["Value2"] = "2"
+        };
+
+        await RazorTemplateEngine.RenderAsync("/Views/ExampleView.cshtml", rclModel, viewData);
+        await RazorTemplateEngine.RenderAsync("/Views/Embedded/EmbeddedResourceView.cshtml", embeddedModel, viewData);
+    }
+
     [Benchmark]
     public async Task RenderViewWithModelAsync()
     {
-        var model = new ExampleModel()
-        {
-            PlainText = "Some text",
-            HtmlContent = "<em>Some emphasized text</em>"
-        };
+        var model = BuildRazorTemplateModel();
         var viewData = new Dictionary<string, object>();
         viewData["Value1"] = "1";
         viewData["Value2"] = "2";
 
         var html = await RazorTemplateEngine.RenderAsync("/Views/ExampleView.cshtml", model, viewData);
     }
+
+    [Benchmark]
+    public async Task RenderEmbeddedResourceViewWithModelAsync()
+    {
+        var model = BuildEmbeddedResourceModel();
+        var viewData = new Dictionary<string, object>();
+        viewData["Value1"] = "1";
+        viewData["Value2"] = "2";
+
+        var html = await RazorTemplateEngine.RenderAsync("/Views/Embedded/EmbeddedResourceView.cshtml", model, viewData);
+    }
+
+
+    private ExampleRazorTemplatesLibrary.Models.ExampleModel BuildRazorTemplateModel()
+    {
+        return new ExampleRazorTemplatesLibrary.Models.ExampleModel()
+        {
+            PlainText = "Some text",
+            HtmlContent = "<em>Some emphasized text</em>"
+        };
+    }
+
+
+    private EmbeddedResourceTemplates.Models.ExampleModel BuildEmbeddedResourceModel()
+    {
+        return new EmbeddedResourceTemplates.Models.ExampleModel()
+        {
+            PlainText = "Some text",
+            HtmlContent = "<em>Some emphasized text</em>"
+        };
+    }
+
 }
---
+++ examples/RazorTemplates/RazorTemplatesLibraryProjectMarker.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleRazorTemplatesLibrary;
+
+/// <summary>
+/// Serves as a marker type to identify this assembly/project.
+/// </summary>
+/// <remarks>This class is intended for use as a project-level marker and does not contain any members or
+/// functionality.</remarks>
+public sealed class RazorTemplatesLibraryProjectMarker
+{
+}
